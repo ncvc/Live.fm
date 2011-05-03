d8cba7b77d7f84598594d76b1d89e12bad4a183f
Nathan V-C <nvcarski@gmail.com>
2011-05-02 20:32:18 -0400

    Finished Main.EventCache and refactored script.js slightly

--- Server/src/Main.py
+++ Server/src/Main.py
@@ -8,13 +8,15 @@ import cgi
 import os
 import logging
 
+import urllib
+
+from datetime import datetime
 from django.utils import simplejson as json
 
 from APIKeys import GoogleMapsAPIKey
 from APIKeys import LastFMAPIKey
 
 from google.appengine.ext.webapp import template
-from google.appengine.api import users
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import db
@@ -24,7 +26,7 @@ class Event(db.Model):
 	eventID = db.IntegerProperty()
 	eventTitle = db.StringProperty()
 	eventDescription = db.TextProperty()
-	eventImageURLs =  db.ListProperty(db.Link)
+	eventImageURLs = db.ListProperty(db.Link)
 	eventAttendance = db.IntegerProperty()
 	eventReviews = db.IntegerProperty()
 	eventTag = db.StringProperty()
@@ -38,12 +40,12 @@ class Event(db.Model):
 	venueURL = db.LinkProperty()
 	venueWebsite = db.LinkProperty()
 	venuePhoneNumber = db.PhoneNumberProperty()
-	venueImageURLs =  db.ListProperty(db.Link)
+	venueImageURLs = db.ListProperty(db.Link)
 	venueAddress = db.PostalAddressProperty()
 	venueTimeZone = db.StringProperty()
 	
 	artists = db.StringListProperty()
-	headliners = db.StringListProperty()
+	headliner = db.StringProperty()
 	
 	tags = db.StringListProperty()
 	
@@ -64,10 +66,6 @@ class MainPage(webapp.RequestHandler):
 	#                else:
 	#                        url = users.create_login_url(self.request.uri)
 	#                        url_linktext = 'Login'
-	
-		#urlfetch.fetch("http://ws.audioscrobbler.com/2.0/",
-		#               headers={"method": "geo.getevents",
-		#                        ""})
 		
 		template_values = {'GoogleMapsAPIKey': GoogleMapsAPIKey}
 		
@@ -83,25 +81,99 @@ class EventCache(webapp.RequestHandler):
 			response = fetch(url)
 		except InvalidURLError as e:
 			logging.error(e)
+			return
 		
 		eventsJSON = json.loads(response)
 		
 		events = self.createEventsFromJSON(eventsJSON)
 		
 		self.addEvents(events)
-		
-	def addEvents(self, events):
-		pass
-	
-	def createEventsFromJSON(self, events):
-		pass
-		#greeting = Greeting()
-		
-		#greeting.content = self.request.get('content')
-		#greeting.put()
 	
 	def buildURL(self, json=True):
-		return None
+		baseURL = 'http://ws.audioscrobbler.com/2.0/'
+		defaultLat = '42.357097'
+		defaultLong = '-71.101523'
+		
+		lat = self.request.get('lat', default_value=None)
+		long = self.request.get('long', default_value=None)
+		dist = 10 #km
+		limit = 10
+		page = 1
+		
+		if lat == None:
+			lat = defaultLat
+		if long == None:
+			long = defaultLong
+		
+		query = {'method': 'geo.getevents',
+				'lat': lat,
+				'long': long,
+				'distance': dist,
+				'limit': limit,
+				'page': page,
+				'api_key': LastFMAPIKey}
+		
+		encoded = urllib.urlencode(query)
+		
+		return '%s?%s' % (baseURL, encoded)
+	
+	def createEventsFromJSON(self, eventsFullJSON):
+		eventsList = []
+		eventsJSON = eventsFullJSON['events']['event']
+		
+		for eventJSON in eventsJSON:
+			event = Event()
+			event.eventID = eventJSON['id']
+			event.eventTitle = eventJSON['title']
+			
+			artistsJSON = eventJSON['artists']
+			event.artists = artistsJSON['artist']
+			event.headliner = artistsJSON['headliner']
+			
+			venueJSON = eventJSON['venue']
+			event.venueID = venueJSON['id']
+			event.venueName = venueJSON['name']
+			
+			locJSON = venueJSON['location']
+			geoJSON = locJSON['geo:point']
+			event.geoPt = db.GeoPt(geoJSON['geo:lat'], geoJSON['geo:long'])
+			line1 = locJSON['street']
+			line2 = '%s, %s %s' % (locJSON['city'], locJSON['country'], locJSON['postalcode'])
+			address = '%s\n%s' % (line1, line2)
+			event.venueAddress = db.PostalAddress(address)
+			
+			event.venueURL = db.Link(venueJSON['url'])
+			event.venueWebsite = db.Link(venueJSON['website'])
+			event.venuePhoneNumber = db.PhoneNumber(venueJSON['phonenumber'])
+			event.venueImageURLs = self.getImageURLs(venueJSON['image'])
+			
+			# Sample string: "Mon, 02 May 2011 22:00:00"
+			formatStr = '%a, %d %b %Y %H:%M:%S'
+			event.date = datetime.strptime(eventJSON['startdate'], formatStr)
+			event.eventDescription = eventJSON['description']
+			event.eventImageURLs = self.getImageURLs(eventJSON['image'])
+			event.eventAttendance = eventJSON['attendance']
+			event.eventReviews = eventJSON['reviews']
+			event.eventTag = eventJSON['tag']
+			event.eventURL = db.Link(eventJSON['url'])
+			event.eventWebsite = db.Link(eventJSON['website'])
+			event.eventTickets = eventJSON['tickets']
+			event.eventCancelled = eventJSON['cancelled']
+			
+			eventsList.append(event)
+		
+		return eventsList
+	
+	def getImageURLs(self, imagesJSON):
+		imageURLs = []
+		for imageJSON in imagesJSON:
+			imageURLs.append(db.Link(imageJSON['#text']))
+		
+		return imageURLs
+		
+	def addEvents(self, events):
+		for event in events:
+			event.put()
 
 application = webapp.WSGIApplication([('/', MainPage)],
                                      debug=True)
@@ -109,5 +181,5 @@ application = webapp.WSGIApplication([('/', MainPage)],
 def main():
 	run_wsgi_app(application)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 	main()
--- Server/src/static/script.js
+++ Server/src/static/script.js
@@ -2,28 +2,38 @@
  * 
  */
 
-var SIMMONS = new google.maps.LatLng(42.357097,-71.101523);
+var SIMMONS = new google.maps.LatLng(42.357097, -71.101523);
 var map;
 var infowindow = new google.maps.InfoWindow();
 
 function initialize() {
-	var myOptions = {
-		zoom: 14,
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
+	createMap();
 	
-	map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+	getLocation();
+}
 
+function createMap() {
+	var myOptions = {
+			zoom: 14,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+			};
+		
+	map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+}
+
+
+function getLocation() {
 	// Try W3C Geolocation method (Preferred)
 	if(navigator.geolocation) {
 		contentString = "Location found using W3C standard";
 		navigator.geolocation.getCurrentPosition(function (position) {
-			showMap(new google.maps.LatLng(position.coords.latitude,position.coords.longitude));
+			
+			showMap(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
 		}, handleNoGeoLocation);
 	} else if (google.gears) {
 		// Try Google Gears Geolocation
 		contentString = "Location found using Google Gears";
-		initialLocation = new google.maps.LatLng(position.latitude,position.longitude);
+		initialLocation = new google.maps.LatLng(position.latitude, position.longitude);
 		var geo = google.gears.factory.create('beta.geolocation');
 		geo.getCurrentPosition(function (position) {
 			showMap(new google.maps.LatLng(position.latitude, position.longitude));
@@ -49,11 +59,11 @@ function showMap(initialLocation) {
 
 function parseXML() {
 	if (window.XMLHttpRequest) {
-		xmlhttp=new XMLHttpRequest();
+		xmlhttp = new XMLHttpRequest();
 	} else {
-		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
 	}
 	xmlhttp.open("GET", "http://ws.audioscrobbler.com/2.0/?method=geo.getevents&location=madrid&api_key=b25b959554ed76058ac220b7b2e0a026", false);
 	xmlhttp.send();
-	contentString=xmlhttp.responseXML;
+	contentString = xmlhttp.responseXML;
 }
